f7839d8a58feeca0acb265d5f4c7dae8d1d2149b
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2025-08-15 18:12:39 +0530

    track input state

--- password-generator/src/app/app.ts
+++ password-generator/src/app/app.ts
@@ -8,7 +8,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
 
   onButtonClick() {
     this.password = 'MY PASSWORD...';
